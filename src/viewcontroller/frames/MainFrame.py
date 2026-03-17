681785e1d2af88f30246c158a99422591d75be48
arthur-georget <arthur.georget@laplateforme.io>
2026-03-17 18:25:37 +0100

    feat(mainframe): New withdraw and deposit buttons added

--- src/viewcontroller/frames/MainFrame.py
+++ src/viewcontroller/frames/MainFrame.py
@@ -5,10 +5,26 @@ class MainFrame(ctk.CTkFrame):
         ctk.CTkFrame.__init__(self, parent)
         
         label = ctk.CTkLabel(self, text ="Main menu", font = ("Arial", 12, "bold"))
-        
         label.grid(row = 0, column = 4, padx = 10, pady = 10) 
  
-        history_button = ctk.CTkButton(self, text ="History",
-        command = lambda : controller.show_frame("HistoryFrame"))
+        transactions_record_button = ctk.CTkButton(self, text ="Transactions record",
+        command = lambda : controller.show_frame("transactions_recordFrame"))
+        transactions_record_button.grid(row = 1, column = 1, padx = 10, pady = 10)
+
+        withdraw_button = ctk.CTkButton(self, text ="Withdraw",
+        command = self.__ask_withdraw_amount)
+        withdraw_button.grid(row = 2, column = 1, padx = 10, pady = 10)
+
+        deposit_button = ctk.CTkButton(self, text ="Deposit",
+        command = self.__ask_deposit_amount)
+        deposit_button.grid(row = 3, column = 1, padx = 10, pady = 10)
+
+
+    def __ask_withdraw_amount():
+        print("Withdraw")
+        pass
+
     
-        history_button.grid(row = 1, column = 1, padx = 10, pady = 10)
+    def __ask_deposit_amount():
+        print("Deposit")
+        pass
