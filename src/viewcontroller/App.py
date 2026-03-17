803f020b83e31df0ab2b7d71d2bbc0c445322728
arthur-georget <arthur.georget@laplateforme.io>
2026-03-17 15:57:19 +0100

    feat(app): New App and frames classes navigation backbone done.

---
+++ src/viewcontroller/App.py
@@ -0,0 +1,40 @@
+import customtkinter as ctk
+
+from src.viewcontroller.frames.UserConnectionFrame import UserConnectionFrame
+from src.viewcontroller.frames.NewAccountFrame import NewAccountFrame
+from src.viewcontroller.frames.MainFrame import MainFrame
+from src.viewcontroller.frames.HistoryFrame import HistoryFrame
+
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
+class App(ctk.CTk):
+
+    def __init__(self, *args, **kwargs):
+
+        ctk.CTk.__init__(self, *args, **kwargs)
+
+        self.title("Budget Buddy")
+        self.geometry("800x600")
+
+        #self.__user = User()
+
+        container = ctk.CTkFrame(self)
+        container.pack(side = "top", fill = "both", expand = True)
+
+        container.grid_rowconfigure(0, weight = 1)
+        container.grid_columnconfigure(0, weight = 1)
+
+        self.frames = {}
+
+        for FrameClass in (UserConnectionFrame, NewAccountFrame, MainFrame, HistoryFrame):
+            frame_instance = FrameClass(container, self)
+            self.frames[FrameClass.__name__] = frame_instance
+
+            frame_instance.grid(row = 0, column = 0, sticky = "nsew")
+        
+        self.show_frame("UserConnectionFrame")
+
+    def show_frame(self, frame_class_name : str):
+        frame = self.frames[frame_class_name]
+        frame.tkraise()
