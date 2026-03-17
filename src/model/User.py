778f8a779ee7ed45e7a9ce0c3420a65511bb9f95
Marius GAVINI <marius.gavini@laplateforme.io>
2026-03-17 16:02:48 +0100

    feat(User): WIP for the User class

---
+++ src/model/User.py
@@ -0,0 +1,128 @@
+from src.model.DataBase import Database
+
+class User():
+    def __init__(self):
+        self.__db = Database()
+
+    #CREATE
+    def create(self, is_admin:bool, firstname:str, lastname:str, email:str, password:str):
+        # create self instance
+        self.__is_admin = is_admin
+        self.__firstname = firstname
+        self.__lastname = lastname
+        self.__email = email
+        self.__password = password
+        # open cursor 
+        cursor = self.__db.get_cursor()
+        
+        sql = """
+        INSERT INTO user (firstname, lastname, email, password, is_admin)
+        VALUES (%s, %s, %s, %s, , %s)
+        """
+        cursor.execute(sql,(self.__firstname, self.__lastname, self.__email, self.__password, self.__is_admin))
+        self.__db.connect.commit()
+        lastrow = cursor.lastrowid
+        
+        # close cursor and database
+        self.__db.close_c()
+        self.__db.close_db()
+        return lastrow
+
+     #READ
+    def read(id :int):
+        pass
+
+    def get_id(self):
+        cursor = self.__db.get_cursor()
+        sql = """
+        SELECT id 
+        FROM user
+        WHERE passwoed =%s and email=%s  
+        """
+        cursor.execute(sql,(self.__lastname, self.__firstname, self.__email))
+        result_id = cursor.fetchone()
+        self.__db.connect.commit()
+        self.__db.close_c()
+        self.__db.close_db()
+        return result_id[0]
+
+    # UPDATE
+    def update_firstname(self, new_name :str, id:int):
+        cursor = self.__db.get_cursor()
+        sql = """
+        UPDATE user
+        SET firstname =%s
+        WHERE id = %s
+        """
+        cursor.execute(sql,(new_name,id))
+        self.__db.connect.commit()
+        self.__db.close_c()
+        self.__db.close_db()  
+        return
+     
+    def update_lastname(self, new_lastname:str, id:int):
+        cursor = self.__db.get_cursor()
+        sql = """
+        UPDATE user
+        SET lastname =%s
+        WHERE id = %s
+        """
+        cursor.execute(sql,(new_lastname,id))
+        self.__db.connect.commit()
+        self.__db.close_c()
+        self.__db.close_db()  
+        return
+
+    def update_email(self, new_email:str, id):
+        cursor = self.__db.get_cursor()
+        sql = """
+        UPDATE user
+        SET email =%s
+        WHERE id = %s
+        """
+        cursor.execute(sql,(new_email,id))
+        self.__db.connect.commit()
+        self.__db.close_c()
+        self.__db.close_db()  
+        return
+
+    def update_password(self, new_password:str):
+        cursor = self.__db.get_cursor()
+        # DON'T FORGET TO HASH PASSWORD
+        
+        sql = """
+        UPDATE user
+        SET password =%s
+        WHERE id = %s
+        """
+        cursor.execute(sql,(new_password,id))
+        self.__db.connect.commit()
+        self.__db.close_c()
+        self.__db.close_db()  
+        return
+    
+    #DELETE
+    def delete(self, id):
+        #SUPPRESSION EN CASCADE A FAIRE
+        cursor = self.db.get_cursor()
+        sql = "DELETE FROM user WHERE id=%s"
+        cursor.execute(sql, (id,))
+        self.db.connect.commit()
+        delete_row = cursor.rowcount
+        self.db.close_c()
+
+        if delete_row == 0:
+            print("ECHEC: Aucun utilisateur trouvé avec cet ID.")  
+        
+        elif delete_row == 1:
+            print("REUSSITE: Utilisateur trouvé avec cet ID.")      
+        return delete_row
+
+    def __instantiate_bank_account():
+        pass
+
+    def __check_login():
+        pass
+
+    def __check_password():
+        pass
