d2087cd0a0a70775315b23f6b22adb7baeff5d5a
arthur-georget <arthur.georget@laplateforme.io>
2026-03-18 11:43:20 +0100

    feat(MainFrame): Added warning, error and confirmation popups after deposit and withdraw

--- src/viewcontroller/frames/MainFrame.py
+++ src/viewcontroller/frames/MainFrame.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from functools import partial
+from CTkMessagebox import CTkMessagebox
 
 class MainFrame(ctk.CTkFrame):
     def __init__(self, parent, controller): 
@@ -13,23 +13,49 @@ class MainFrame(ctk.CTkFrame):
         transactions_record_button.grid(row = 1, column = 1, padx = 10, pady = 10)
 
         withdraw_button = ctk.CTkButton(self, text ="Withdraw",
-        command = partial(self.__ask_amount,"withdraw"))
+        command = lambda : self.__ask_amount("withdraw"))
         withdraw_button.grid(row = 2, column = 1, padx = 10, pady = 10)
 
         deposit_button = ctk.CTkButton(self, text ="Deposit",
-        command = partial(self.__ask_amount,"deposit"))
+        command = lambda : self.__ask_amount("deposit"))
         deposit_button.grid(row = 3, column = 1, padx = 10, pady = 10)
 
 
     def __ask_amount(self, operation_type : str):
-        dialog = ctk.CTkInputDialog(text=f"How much money do you want to {operation_type}?", title=f"{operation_type}")
-        try:
-            amount = float(dialog.get_input())
-            if amount < 0:
-                print("The amount provided must be > 0.")
-                self.__ask_amount(operation_type)
-            else:    
-                print(amount)
-        except:
-            print("The amount provided must be a number.")
-            self.__ask_amount(operation_type)
+        dialog = ctk.CTkInputDialog(text=f"How much money do you want to {operation_type}?", title=f"{operation_type.title()}")
+        user_input = dialog.get_input()
+        if user_input is not None:
+            try:
+                amount = float(user_input)
+                if amount < 0:
+                    warning_message = CTkMessagebox(title="Error: Amount provided is negative", 
+                                              message="The amount provided should be positive.", 
+                                              icon="warning",
+                                              option_1="Cancel",
+                                              option_2="Retry")
+                    if warning_message.get()=="Retry":
+                        self.__ask_amount(operation_type)
+                else:
+                    try:
+                        # REPLACE THIS COMMENT WITH User.BankAccount.new_transaction()
+                        # AND User.BankAccount.update(new_balance)
+                        CTkMessagebox(title=f"{operation_type.title()} success.",
+                                                           message=f"{amount}€ {operation_type} done successfully.",
+                                                           icon="check")
+                    except:
+                        error_message = CTkMessagebox(title=f"{operation_type.title()} error",
+                                                      message=f"Something went wrong during {operation_type}.",
+                                                      icon="cancel",
+                                                      option_1="Cancel",
+                                                      option_2="Retry")
+                        if error_message.get() == "Retry":
+                            self.__ask_amount(operation_type)
+                    
+            except:
+                warning_message = CTkMessagebox(title="Error: Amount provided is not a number.", 
+                                              message="The amount provided should be a number.", 
+                                              icon="warning",
+                                              option_1="Cancel",
+                                              option_2="Retry")
+                if warning_message.get()=="Retry":
+                    self.__ask_amount(operation_type)
