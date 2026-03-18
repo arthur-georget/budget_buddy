2cc7bf7f02d3bc4a3145d9d98c5219c6e8e72c08
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2026-03-18 10:50:10 +0100

    feat(popup): integrate CTkMessagebox for transfer confirmation

--- src/viewcontroller/popups/TransferPopUp.py
+++ src/viewcontroller/popups/TransferPopUp.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from CTkMessagebox import CTkMessagebox
 
 class TransferPopUp(ctk.CTkToplevel):
     def __init__(self, parent, bankaccount):
@@ -55,11 +56,13 @@ class TransferPopUp(ctk.CTkToplevel):
 
     def call_create_transaction(self):
         is_valid, msg = self.verify_inputs()
+        
         if not is_valid:
             self.error_label.configure(text=msg)
             return
 
-        self.error_label.configure(text="")
-        print(f"Transfer: {self.amount_input.get()} -> {self.target_account_id_input.get()}")
-        self.destroy()
         
+        CTkMessagebox(title="Success", message="Transfer initiated successfully!", icon="check", option_1="Close")
+        
+        
+        self.destroy()
