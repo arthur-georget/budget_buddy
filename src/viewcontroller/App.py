2988e8842f74a710f709baaa4515f81f8a932617
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2026-03-18 17:28:04 +0100

    refactor(app): final adjustments to window size and imports

--- src/viewcontroller/App.py
+++ src/viewcontroller/App.py
@@ -4,29 +4,31 @@ from src.viewcontroller.frames.NewUserFrame import NewUserFrame
 from src.viewcontroller.frames.MainFrame import MainFrame
 from src.viewcontroller.frames.TransactionsRecordFrame import TransactionsRecordFrame
 
+ctk.set_appearance_mode("System")
+ctk.set_default_color_theme("blue")
+
 class App(ctk.CTk):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        ctk.CTk.__init__(self, *args, **kwargs)
+
         self.title("Budget Buddy")
-        self.geometry("900x650")
-        
-        ctk.set_appearance_mode("System")
-        ctk.set_default_color_theme("blue")
+        self.geometry("1000x700")
 
         container = ctk.CTkFrame(self)
         container.pack(side="top", fill="both", expand=True)
+
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        for F in (UserConnectionFrame, NewUserFrame, MainFrame, TransactionsRecordFrame):
-            frame = F(container, self)
-            self.frames[F.__name__] = frame
-            frame.grid(row=0, column=0, sticky="nsew")
+
+        for FrameClass in (UserConnectionFrame, NewUserFrame, MainFrame, TransactionsRecordFrame):
+            frame_instance = FrameClass(container, self)
+            self.frames[FrameClass.__name__] = frame_instance
+            frame_instance.grid(row=0, column=0, sticky="nsew")
         
         self.show_frame("UserConnectionFrame")
 
-    def show_frame(self, page_name):
-        frame = self.frames[page_name]
+    def show_frame(self, frame_class_name : str):
+        frame = self.frames[frame_class_name]
         frame.tkraise()
-        frame.focus_force()
