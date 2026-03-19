bc42bcb58dea70dbb301bfc912ec098b946d52c0
arthur-georget <arthur.georget@laplateforme.io>
2026-03-19 17:44:54 +0100

    feat(frames): New ScrollableFilterFrame class. Added an instance of it in TransactionsRecordFrame.

---
+++ src/viewcontroller/frames/ScrollableFilterFrame.py
@@ -0,0 +1,38 @@
+import customtkinter as ctk
+from functools import partial
+
+class ScrollableFilterFrame(ctk.CTkScrollableFrame):
+
+    def __init__(self, parent, filters: list, title: str, radio: bool = False, **kwargs):
+
+        ctk.CTkScrollableFrame.__init__(self, parent, label_text=title, **kwargs)
+
+        self.__parent = parent
+
+        self.__radio = radio
+
+        self._scrollbar.configure(height=0)
+        
+        if self.__radio:
+            self.__radio_var = ctk.StringVar(value="")
+            for i,filter in enumerate(filters):
+                filter_button = ctk.CTkRadioButton(self, text=filter, variable= self.__radio_var, value=filter)
+                filter_button.grid(row=i+1, column=0, sticky="W", pady=5)
+        else:        
+            all_filter_button = ctk.CTkButton(self, text='Tous', command= partial(self.__filter_action,None), width=20)
+            all_filter_button.grid(row=0, column=0, sticky="W", pady=5)
+            for i,filter in enumerate(filters):
+                filter_button = ctk.CTkButton(self, text=filter, command= partial(self.__filter_action,filter), width=20)
+                filter_button.grid(row=i+1, column=0, sticky="W", pady=5)
+            
+        self.__selected_filter = None
+
+
+    def __filter_action(self, filter: str | None):
+        self.__selected_filter = filter
+        self.__parent.filter_action(None)
+    
+    def get_selected_filter(self):
+        if self.__radio:
+            self.__selected_filter = self.__radio_var.get()
+        return self.__selected_filter
