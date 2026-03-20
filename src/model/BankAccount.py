7d6424ef608b80c0d8bf23b06522a978291fef92
Marius GAVINI <marius.gavini@laplateforme.io>
2026-03-20 15:37:06 +0100

    feat(bank_account): create done and read_withansction wip

--- src/model/BankAccount.py
+++ src/model/BankAccount.py
@@ -14,9 +14,18 @@ class BankAccount:
         return self.__balance
     
 
-    def create(self):
-        pass
-
+    def create(self, id_user:int, balance_account = 1):
+        cursor = self.__db.get_cursor()
+        
+        sql = """ INSERT INTO bank_account (id, balance)
+        VALUES (%s, %s)
+        """
+        cursor.execute(sql,(id_user, balance_account))
+        self.__db.connect.commit()
+        lastrow = cursor.lastrowid
+        self.__db.close_c()
+        self.__db.close_db()
+        return lastrow
 
     def read(self, id: int):
 
@@ -34,6 +43,20 @@ class BankAccount:
         self.__db.close_c()
         self.__db.close_db()
 
+    def read_with_transaction(self, id_user):
+        cursor = self.__db.get_cursor()
+        sql = """
+        SELECT balance, transaction
+        FROM bank_account
+        WHERE id = %s
+        """
+        cursor.execute(sql,(id_user,))
+        result = cursor.fetchone()
+        self.__id = id
+        self.__balance = result[0]
+
+        self.__db.close_c()
+        self.__db.close_db()
 
     def update(self):
         pass
