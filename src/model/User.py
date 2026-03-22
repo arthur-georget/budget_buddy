031e2de7c80ca5f3876703e80303390ce771ab7d
Marius GAVINI <marius.gavini@laplateforme.io>
2026-03-22 18:33:42 +0100

    refactor(user): refactor user methods

--- src/model/User.py
+++ src/model/User.py
@@ -5,16 +5,20 @@ class User():
 
     def __init__(self):
         self.__db = Database()
-        
+        self.__id = None
+        self.__firstname = None
+        self.__lastname = None
+        self.__email = None
+        self.__is_admin = None
+        self.__bank_accounts = []
 
+    # GETTERS
     def get_email(self):
         return self.__email
-
 
     def get_bank_accounts(self):
         return self.__bank_accounts
     
-
     #CREATE
     def create(self, is_admin:bool, firstname:str, lastname:str, email:str, password:str):
         # create self instance
@@ -23,24 +27,33 @@ class User():
         self.__lastname = lastname
         self.__email = email
         self.__password = self.__db.hash_password(password)
-        # open cursor 
+
         cursor = self.__db.get_cursor()
         
         sql = """
         INSERT INTO user (firstname, lastname, email, password, is_admin)
         VALUES (%s, %s, %s, %s, %s)
         """
-        cursor.execute(sql,(self.__firstname, self.__lastname, self.__email, self.__password, self.__is_admin))
-        self.__db.connect.commit()
-        lastrow = cursor.lastrowid
+        cursor.execute(sql,(
+            self.__firstname, 
+            self.__lastname, 
+            self.__email, 
+            self.__password, 
+            self.__is_admin))
         
-        # close cursor and database
-        self.__db.close_c()
-        # need to instanciate bank_account with transaction and balance
-        self.__bank_accounts = [BankAccount()]
-        self.__bank_accounts[0].create()
-        self.__db.close_db()
-        return lastrow
+        self.__db.connect.commit()
+        self.__id = cursor.lastrowid
+        cursor.close
+        # OLD
+        #self.__bank_accounts = [BankAccount()]
+        #self.__bank_accounts[0].create()
+        #self.__db.close_db()
+        
+        # NEW
+        account = BankAccount()
+        account.create()
+        self.__bank_accounts =[account]
+        return self.__id
     
 
     def login(self, email, password):
@@ -50,7 +63,7 @@ class User():
         else:
             print(id_user)
 
-
+    # READ
     def read(self, id:int):
         cursor = self.__db.get_cursor()
         sql = """
@@ -60,75 +73,47 @@ class User():
         """
         cursor.execute(sql,(id,))
         result = cursor.fetchone()
+        cursor.close()
+        if result is None:
+            return None
+        
+        self.__id = id
         self.__firstname = result[0]
         self.__lastname = result[1]
         self.__email = result[2]
         self.__is_admin = result[3]
-        self.__id = id
         self.__instantiate_bank_accounts()
-        return [self.__id, self.__firstname, self.__lastname, self.__email, self.__is_admin, self.__bank_accounts] 
+        return [self.__id,
+                self.__firstname,
+                self.__lastname,
+                self.__email,
+                self.__is_admin,
+                self.__bank_accounts] 
 
-### NEED TO BE REFACTORED ###
-    def update_firstname(self, new_name :str):
+    # UPDATE CONTROLLER
+    def __update_field(self, field: str, value):
         cursor = self.__db.get_cursor()
-        sql = """
-        UPDATE user
-        SET firstname =%s
-        WHERE id = %s
-        """
-        cursor.execute(sql,(new_name,self.__id))
+
+        sql = f"UPDATE user SET {field} = %s WHERE id = %s"
+        cursor.execute(sql, (value, self.__id))
         self.__db.connect.commit()
-        self.__db.close_c()
-        self.__db.close_db()  
-        return
-     
+        cursor.close()
+    # UPDATERS
+    def update_firstname(self, new_name: str):
+        self.__update_field("firstname", new_name)
+        self.__firstname = new_name
 
-    def update_lastname(self, new_lastname:str, id:int):
-        cursor = self.__db.get_cursor()
-        sql = """
-        UPDATE user
-        SET lastname =%s
-        WHERE id = %s
-        """
-        cursor.execute(sql,(new_lastname,id))
-        self.__db.connect.commit()
-        self.__db.close_c()
-        self.__db.close_db()  
-        return
+    def update_lastname(self, new_lastname: str):
+        self.__update_field("lastname", new_lastname)
+        self.__lastname = new_lastname
 
+    def update_email(self, new_email: str):
+        self.__update_field("email", new_email)
+        self.__email = new_email
 
-    def update_email(self, new_email:str, id):
-        cursor = self.__db.get_cursor()
-        sql = """
-        UPDATE user
-        SET email =%s
-        WHERE id = %s
-        """
-        cursor.execute(sql,(new_email,id))
-        self.__db.connect.commit()
-        self.__db.close_c()
-        self.__db.close_db()  
-        return
-### NEED TO BE REFACTORED END ###
- 
-
-    def update_password(self, new_password:str):
-
-        cursor = self.__db.get_cursor()
-        # DON'T FORGET TO HASH PASSWORD
-        new_password = self.__db.hash_password(new_password)
-
-        sql = """
-        UPDATE user
-        SET password =%s
-        WHERE id = %s
-        """
-        cursor.execute(sql,(new_password,self.__id))
-        self.__db.connect.commit()
-        self.__db.close_c()
-        self.__db.close_db()  
-        return
-    
+    def update_password(self, new_password: str):
+        hashed = self.__db.hash_password(new_password)
+        self.__update_field("password", hashed)
 
     def delete(self, id):
         #DEL CASCADE !
@@ -136,19 +121,18 @@ class User():
         sql = "DELETE FROM user WHERE id=%s"
         cursor.execute(sql, (id,))
         self.__db.connect.commit()
-        delete_row = cursor.rowcount
-        self.__db.close_c()
+        deleted = cursor.rowcount
+        cursor.close()
 
-        if delete_row == 0:
-            print("ECHEC: Aucun utilisateur trouvé avec cet ID.")  
+        if deleted == 0:
+            print("FAIL: No users were found with this ID.")  
         
-        elif delete_row == 1:
-            print("REUSSITE: Utilisateur trouvé avec cet ID.")      
-        return delete_row
+        elif deleted == 1:
+            print("SUCCESS: User found with this ID.")      
+        return deleted
 
 
     def __instantiate_bank_accounts(self):
-
         self.__bank_accounts = []
         cursor = self.__db.get_cursor()
         sql = """
@@ -158,7 +142,7 @@ class User():
         """
         cursor.execute(sql,(self.__id,))
         results = cursor.fetchall()
-
+        cursor.close()
         for result in results:
             bank_account = BankAccount()
             bank_account.read(result[0])
